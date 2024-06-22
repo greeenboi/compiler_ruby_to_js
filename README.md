e6f0534830c93730425c8b82b6c6167df80c19f0
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-06-22 19:53:36 +0530

    first commit : tokenizer

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ruby Compiler
+
+Contains a tokenizer, a parser, and a code generator.
+
+By the end, it successfully compiles some code in our language `test.src`
+, producing JavaScript output that we can execute. 
+
+While most compilers are highly optimized for speed, but ours will be optimized for easy readability and understanding.
---
+++ compiler.rb
@@ -0,0 +1,43 @@
+class Tokenizer
+  TOKEN_TYPES = [
+    [:def, /\bdef\b/],
+    [:end, /\bend\b/],
+    [:identifier,/\b[a-zA-Z]+\b/],
+    [:integer,/\b[0-9]+\b/],
+    [:oparen,/\(/],
+    [:cparen,/\)/]
+  ]
+  def initialize(code)
+    @code = code
+  end
+
+  def tokenize
+    tokens = []
+    until @code.empty?
+      tokens << tokenize_one_token
+      @code = @code.strip
+    end
+    tokens
+  end
+
+  def tokenize_one_token
+    TOKEN_TYPES.each do |type, re|
+      re = /\A(#{re})/
+      if @code =~ re
+        value = $1
+        @code = @code[value.length..-1]
+        return Token.new(type,value)
+      end
+    end
+    raise RuntimeError.new(
+      "Unexpected character #{@code.inspect}"
+    )
+
+  end
+end
+
+Token = Struct.new(:type, :value)
+
+tokens = Tokenizer.new(File.read("test.src")).tokenize
+
+puts(tokens.map(&:inspect).join("\n"))
